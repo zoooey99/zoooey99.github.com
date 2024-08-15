cb58f4c210059acdb4cf9db8c7cd86b89173ac98
zoooey99 <zoeyjleeweb@gmail.com>
2024-08-15 08:54:15 -0400

    added buttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
+import { Button } from "@/components/ui/button"
+
 export default function Home(){
   return (
-  <div>
+  <div className="justify-items-center">
     <h1>Hi my name is Zoey Lee.</h1>
+    <Button>linkedin</Button>
+    <Button>email</Button>
+    <Button>instagram</Button>
   </div>
   )}
