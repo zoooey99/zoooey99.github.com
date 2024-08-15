18c80beb78bf8606479fa65ece15720e4ccaafc7
zoooey99 <zoeyjleeweb@gmail.com>
2024-08-15 10:13:29 -0400

    background

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,21 @@ import { Button } from "@/components/ui/button"
 
 export default function Home(){
   return (
-  <div className="justify-items-center">
-    <h1>Hi my name is Zoey Lee.</h1>
-    <Button>linkedin</Button>
-    <Button>email</Button>
-    <Button>instagram</Button>
-  </div>
+    <div>
+
+      <div className="bg-hero bg-center h-screen bg-fixed">
+        <div className="text-center absolute inset-x-1/2 inset-y-1/2 translate-x-1/2 translate-y-1/2">
+          <h1 className="text-lg">Hi my name is Zoey Lee.</h1>
+          <Button>linkedin</Button>
+          <Button>email</Button>
+          <Button>instagram</Button>
+        </div>
+      </div>
+
+      <div>
+        hello more text
+      </div>
+
+    </div>
+    
   )}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -72,6 +72,9 @@ const config = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      backgroundImage: {
+        'hero': "url('../public/images/scuba.jpg')",
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
