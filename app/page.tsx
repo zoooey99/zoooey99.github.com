512b8faf772ad917ff5fdd70e73bace510f7aae9
zoooey99 <zoeyjleeweb@gmail.com>
2024-08-16 09:17:15 -0400

    HERO: icons updated, text vetically centered but not horizontally centered :/

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,27 @@
 import { Button } from "@/components/ui/button"
+import Link from "next/link"
+import { Mail, Linkedin, Instagram } from "lucide-react"
+
 
 export default function Home(){
   return (
     <div>
 
-      <div className="bg-hero bg-center h-screen bg-fixed">
-        <div className="text-center absolute inset-x-1/2 inset-y-1/2 translate-x-1/2 translate-y-1/2">
-          <h1 className="text-lg">Hi my name is Zoey Lee.</h1>
-          <Button>linkedin</Button>
-          <Button>email</Button>
-          <Button>instagram</Button>
-        </div>
+      <div className="bg-hero bg-center bg-fixed flex items-center h-screen">
+          <h1 className="text-2xl text-white text-center mg-auto">Hi my name is Zoey Lee.</h1>
       </div>
 
-      <div>
-        hello more text
+      <div className="flex text-center">
+        <Button asChild>
+            <Link href="https://www.linkedin.com/in/zoeylee123/"><Linkedin className="h-4 w-4"/></Link>
+          </Button>
+          <Button asChild>
+            <Link href="mailto:zjl24@miami.edu"><Mail className="h-4 w-4"/></Link>
+          </Button>
+          <Button asChild>
+            <Link href="https://www.instagram.com/zoeylee.11/"><Instagram className="h-4 w-4"/></Link>
+          </Button>
+
       </div>
 
     </div>
