10abe723a9594f458d9f4fc175d6adf9f9a7d587
zoooey99 <zoeyjleeweb@gmail.com>
2024-10-26 07:40:44 -0400

    added projects and website status pages

---
+++ app/projects/page.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+export default function projects(){
+    return(
+        <div>
+            <h1> here is my projects page! </h1>
+            <Link href="/"> click here to go back to home</Link>
+        </div>
+        
+    )
+}
