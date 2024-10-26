3e7a576243cc5b81b991f28b50ee33b5c78a69c9
zoooey99 <zoeyjleeweb@gmail.com>
2024-10-26 08:02:18 -0400

    added nav bar with 4 pages. Links to all pages active

--- app/page.tsx
+++ app/page.tsx
@@ -9,15 +9,74 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel"
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu"
+import { navigationMenuTriggerStyle } from "@/components/ui/navigation-menu"
+
+
 
 export default function Home() {
   return (
     <div className="font-['Source_Sans_Pro',_sans-serif]">
+      {/* navigation */}
+      <NavigationMenu>
+        <NavigationMenuList>
+        
+        {/* about page */}
+        <NavigationMenuItem>
+          <Link href="/about" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              About
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+
+        {/* projects page */}
+        <NavigationMenuItem>
+          <Link href="/projects" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Projects
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+          
+        {/* website status page */}  
+        <NavigationMenuItem>
+          <Link href="/website-status" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Website Status
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+
+        {/* stats page */}
+        <NavigationMenuItem>
+          <Link href="/stats" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Stats
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+
+        </NavigationMenuList>
+      </NavigationMenu>
+
+      {/* header */}
       <div className="bg-hero bg-center bg-fixed flex items-end justify-center h-screen pb-[15vh]">
         <h1 className="text-5xl md:text-6xl lg:text-7xl text-white text-center font-light">
           <span className="font-bold">HI!</span> my name is <span className="font-bold">Zoey Lee</span>
         </h1>
       </div>
+
+      {/* picture carousel */}
       <div className="flex justify-center items-center py-8">
         <Carousel className="w-full max-w-5xl">
           <CarouselContent className="-ml-2 md:-ml-4">
@@ -44,6 +103,8 @@ export default function Home() {
           <CarouselNext />
         </Carousel>
       </div>
+
+      {/* personal links */}
       <div className="flex justify-center mt-4 pb-8">
         <Button asChild className="mx-2">
           <Link href="https://www.linkedin.com/in/zoeylee123/">
@@ -61,7 +122,6 @@ export default function Home() {
           </Link>
         </Button>
       </div>
-      <h1> <Link href="/about">about</Link></h1>
     </div>
   )
 }
