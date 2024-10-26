0336137f69a693a1fe4a35c97206649954d882fd
zoooey99 <zoeyjleeweb@gmail.com>
2024-10-26 19:21:31 -0400

    removed zoey lee home button

--- components/nav-bar-global.tsx
+++ components/nav-bar-global.tsx
@@ -1,66 +1,62 @@
+
 import {
   NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
 } from "@/components/ui/navigation-menu"
 import { navigationMenuTriggerStyle } from "@/components/ui/navigation-menu"
 import Link from "next/link"
 
-export default function NavbarGlobal() {
-  return (
-    <nav className="bg-transparent absolute top-0 left-0 right-0 z-10 p-4">
-      <div className="container mx-auto flex justify-between items-center">
-        {/* Zoey Lee button on the left */}
-        <NavigationMenu>
-          <NavigationMenuList>
-            <NavigationMenuItem>
-              <Link href="/" legacyBehavior passHref>
-                <NavigationMenuLink className={`${navigationMenuTriggerStyle()}`}>
-                  Zoey Lee
-                </NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu>
+export default function NavbarGlobal(){
+  return(
+      <div>
+          {/* navigation */}
+    <NavigationMenu className="bg-transparent absolute top-4 right-4 w-full z-10">
+      <NavigationMenuList>
+      
+      {/* about page */}
+      <NavigationMenuItem>
+        <Link href="/about" legacyBehavior passHref>
+          <NavigationMenuLink className={`${navigationMenuTriggerStyle()}`}>
+            About
+          </NavigationMenuLink>
+        </Link>
+      </NavigationMenuItem>
 
-        {/* Other navigation items on the right */}
-        <NavigationMenu>
-          <NavigationMenuList className="flex space-x-2">
-            <NavigationMenuItem>
-              <Link href="/about" legacyBehavior passHref>
-                <NavigationMenuLink className={`${navigationMenuTriggerStyle()}`}>
-                  About
-                </NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
+      {/* projects page */}
+      <NavigationMenuItem>
+        <Link href="/projects" legacyBehavior passHref>
+          <NavigationMenuLink className={`${navigationMenuTriggerStyle()} outline outline-2 outline-offset-2 outline-white`}>
+            Projects
+          </NavigationMenuLink>
+        </Link>
+      </NavigationMenuItem>
+        
+      {/* website status page */}  
+      <NavigationMenuItem>
+        <Link href="/website-status" legacyBehavior passHref>
+          <NavigationMenuLink className={`${navigationMenuTriggerStyle()}`}>
+            Website Status
+          </NavigationMenuLink>
+        </Link>
+      </NavigationMenuItem>
 
-            <NavigationMenuItem>
-              <Link href="/projects" legacyBehavior passHref>
-                <NavigationMenuLink className={`${navigationMenuTriggerStyle()} outline outline-2 outline-offset-2 outline-white`}>
-                  Projects
-                </NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
-              
-            <NavigationMenuItem>
-              <Link href="/website-status" legacyBehavior passHref>
-                <NavigationMenuLink className={`${navigationMenuTriggerStyle()}`}>
-                  Website Status
-                </NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
+      {/* stats page */}
+      <NavigationMenuItem>
+        <Link href="/stats" legacyBehavior passHref>
+          <NavigationMenuLink className={`${navigationMenuTriggerStyle()}`}>
+            Stats
+          </NavigationMenuLink>
+        </Link>
+      </NavigationMenuItem>
 
-            <NavigationMenuItem>
-              <Link href="/stats" legacyBehavior passHref>
-                <NavigationMenuLink className={`${navigationMenuTriggerStyle()}`}>
-                  Stats
-                </NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu>
+      </NavigationMenuList>
+    </NavigationMenu>
       </div>
-    </nav>
   )
 }
